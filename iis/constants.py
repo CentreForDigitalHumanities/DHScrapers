0b25c2b8c56fd0f9b6619c4e9dce095634545e44
BeritJanssen <berit.janssen@gmail.com>
2022-06-01 14:48:32 +0200

    update Brown API URL

--- iis/constants.py
+++ iis/constants.py
@@ -1,3 +1,3 @@
 
-IIS_BASE_URL = 'https://library.brown.edu/cds/projects/iip/view_xml/'
+IIS_BASE_URL = 'https://library.brown.edu/iip/view_xml/'
 ZOTERO_BASE_URL = 'https://api.zotero.org/groups/180188/items?tag={}&include=bib&style=apa'
