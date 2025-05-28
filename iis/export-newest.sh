2dea986c42a0e4c576273669a4047e6c060bd35d
Giorgos Damaskos <g.damaskos@uu.nl>
2025-05-28 13:24:03 +0200

    fix: update export-newest.sh to use current_run directory for output files

--- iis/export-newest.sh
+++ iis/export-newest.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-mkdir /iis-harvester-data/harvest-metadata
-mkdir /iis-harvester-data/iis-files
+mkdir /current_run/harvest-metadata
+mkdir /current_run/iis-files
 git fetch
-export FILE="/iis-harvester-data/harvest-metadata/harvested-files.txt"
+export FILE="/current_run/harvest-metadata/harvested-files.txt"
 git status --porcelain epidoc-files > $FILE
 FILE_LENGTH=$(wc -l <"$FILE")
 if [ "$FILE_LENGTH" -gt 0 ]; then
     git checkout
-    mv epidoc-files/*.xml /iis-harvester-data/iis-files/
+    mv epidoc-files/*.xml /current_run/iis-files/
 fi
