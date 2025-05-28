083b8af4dbcf2def1e029856e825227d8d325a8b
Giorgos Damaskos <g.damaskos@uu.nl>
2025-05-28 12:37:03 +0200

    fix: update paths in Dockerfile and export-newest.sh for consistency

--- iis/export-newest.sh
+++ iis/export-newest.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+mkdir /iis-harvester-data/harvest-metadata
+mkdir /iis-harvester-data/iis-files
 git fetch
-export FILE="/harvest-metadata/harvested-files.txt"
+export FILE="/iis-harvester-data/harvest-metadata/harvested-files.txt"
 git status --porcelain epidoc-files > $FILE
 FILE_LENGTH=$(wc -l <"$FILE")
 if [ "$FILE_LENGTH" -gt 0 ]; then
     git checkout
-    mv epidoc-files/*.xml /iis-files/
+    mv epidoc-files/*.xml /iis-harvester-data/iis-files/
 fi
