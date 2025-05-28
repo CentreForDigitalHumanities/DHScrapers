2347ae1760870f11b5c975d0e7730e6938de51b6
Giorgos Damaskos <g.damaskos@uu.nl>
2025-05-28 16:02:52 +0200

    fix: remove unnecessary directory creation and update file paths in export-newest.sh

--- iis/export-newest.sh
+++ iis/export-newest.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-mkdir /current_run/harvest-metadata
-mkdir /current_run/iis-files
 git fetch
-export FILE="/current_run/harvest-metadata/harvested-files.txt"
+export FILE="/harvest-metadata/harvested-files.txt"
 git status --porcelain epidoc-files > $FILE
 FILE_LENGTH=$(wc -l <"$FILE")
 if [ "$FILE_LENGTH" -gt 0 ]; then
     git checkout
-    mv epidoc-files/*.xml /current_run/iis-files/
+    mv epidoc-files/*.xml /iis-files/
 fi
