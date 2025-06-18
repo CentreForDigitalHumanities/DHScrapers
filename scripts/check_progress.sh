e5ce8077d3f1a0162a15579e2d161daef23887fd
BeritJanssen <berit.janssen@gmail.com>
2025-06-18 16:26:38 +0200

    fix: adjust volume names

--- scripts/check_progress.sh
+++ scripts/check_progress.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # compare the output and postprocessed directories
-export $TIMESTAMP=(%Y%m%d_%H%M%S)
-cd /output/inprogress/
-ls *.xml > ~/enriched.txt
-cd /postprocessed/inprogress
-ls *.xml > ~/postprocessed.txt
-comm -12 ~/enriched.txt ~/postprocessed.txt > ~/finished.txt
-# move all files that are shared between output and postprocessed to $JOB-finished
-mkdir /output/$TIMESTAMP
-cd /output/inprogress/
-for file in $(cat ~/finished.txt); do mv "$file" /output/$TIMESTAMP; done
-# now in the next step, only files which haven't been postprocessed yet will be in the /output/inprogress directory
+export TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+echo $TIMESTAMP
+cd /output
+ls *.xml > /metadata/enriched.txt
+cd /postprocessed
+ls *.xml > /metadata/postprocessed.txt
+comm -12 /metadata/enriched.txt /metadata/postprocessed.txt > /metadata/finished.txt
+FILE_LENGTH=$(wc -l </metadata/finished.txt)
+if [ "$FILE_LENGTH" -gt 0 ]; then
+    # move all files that are shared between output and postprocessed to a timestamped subdirectory
+    mkdir /output/$TIMESTAMP
+    cd /output
+    for file in $(cat /metadata/finished.txt); do mv "$file" /output/$TIMESTAMP; done
+    # now in the next step, only files which haven't been postprocessed yet will be in the /output root directory
+fi
